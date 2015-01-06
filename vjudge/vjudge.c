89dcd9c863162ea103de69ad15a0ce27d29cbe61
gwq <gwq5210@qq.com>
2015-01-06 23:39:18 +0800

    修改vjudge.c

--- vjudge/vjudge.c
+++ vjudge/vjudge.c
@@ -1,5 +1,5 @@
 /*************************************************************************
-	> File Name: hdu_judge.c
+	> File Name: vjudge.c
 	> Author: gwq
 	> Mail: gwq5210@qq.com 
 	> Created Time: 2015年01月06日 星期二 22时07分52秒
@@ -9,5 +9,7 @@
 
 int main(int argc, char *argv[])
 {
+	login();
+	submit();
 	return 0;
 }
