cf4e8f7e6f62515b15e4731650868c5b2d10a5be
gwq <gwq5210@qq.com>
2015-01-10 20:54:20 +0800

    change file make.sh

--- vjudge/core/make.sh
+++ vjudge/core/make.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd judged
 make
 sudo chmod +x judged
 sudo cp judged /usr/bin
+make clean
+cd ../judge_client
+make
 sudo chmod +x judge_client
 sudo cp judge_client /usr/bin
 make clean
