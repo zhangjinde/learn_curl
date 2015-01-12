831b4fe5383d9a89252082b863a5eb09e4b7bd02
gwq <gwq5210@qq.com>
2015-01-12 18:56:48 +0800

    add files

--- vjudge/install/install.sh
+++ vjudge/install/install.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+#CENTOS/REDHAT/FEDORA WEBBASE=/var/www/html APACHEUSER=apache 
+WEBBASE=/var/www/html
+APACHEUSER=www-data
+DBUSER=root
+DBPASS=1234
+
+#create user and homedir
+sudo  /usr/sbin/useradd -m -u 1536 judge
+
+#create work dir set default conf
+sudo    mkdir /home/judge
+sudo    mkdir /home/judge/etc
+sudo    mkdir /home/judge/data
+sudo    mkdir /home/judge/log
+sudo    mkdir /home/judge/run0
+sudo    mkdir /home/judge/run1
+sudo    mkdir /home/judge/run2
+sudo    mkdir /home/judge/run3
+sudo cp java0.policy  judge.conf /home/judge/etc
+sudo chown -R judge /home/judge
+sudo chgrp -R $APACHEUSER /home/judge/data
+sudo chgrp -R root /home/judge/etc /home/judge/run?
+sudo chmod 775 /home/judge /home/judge/data /home/judge/etc /home/judge/run?
+
+cd ../core
+sudo ./make.sh
+
 #boot up judged
 sudo cp judged /etc/init.d/judged
 sudo chmod +x  /etc/init.d/judged
